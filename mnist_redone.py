40b8715aab5cd3eef174eec0f5051b8ecc456d5b
Gregory Adler <greg7239@gmail.com>
2017-05-04 14:15:09 -0400

    regression working

--- mnist_redone.py
+++ mnist_redone.py
@@ -20,3 +20,21 @@ sess.run(tf.global_variables_initializer())
 # regression model - 
 # multiply the image input x the weight matrix (w), then add the result bias (b)
 y = tf.matmul(x,W) + b
+
+
+# loss function
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+for _ in range(1000):
+  batch = mnist.train.next_batch(100)
+  train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
